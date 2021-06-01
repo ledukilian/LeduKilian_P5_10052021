35aacb54a3fb0bdb6b1ed185ffc92ea3af0c9785
ledukilian <ledu.kilian@gmail.com>
2021-06-01 04:32:59 +0200

    Progress

--- src/Controllers/IndexController.php
+++ src/Controllers/IndexController.php
@@ -6,10 +6,12 @@ use App\Core\Controller;
 class IndexController extends Controller {
    public function showHome() {
       echo 'Home.';
+      // Ici on ira chercher la vue de l'accueil
    }
 
    public function showContact() {
       echo 'Contact.';
+      // Ici on ira chercher la vue de contact
    }
 
 
