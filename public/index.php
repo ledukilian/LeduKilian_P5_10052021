d8b4ba88ea360b6594c093c6a3fac616cf21d7aa
Kilian LE DU <ledu.kilian@gmail.com>
2021-06-06 21:25:55 +0200

    Added basic routes for Twig

--- public/index.php
+++ public/index.php
@@ -4,6 +4,10 @@ use App\Core\Router;
 
 define('ROOT_DIR', realpath(dirname(__DIR__)));
 define('CONF_DIR', realpath(dirname(__DIR__)) . '/config');
+define('TEMPLATE_DIR', realpath(dirname(__DIR__)) . '/templates');
+
+
+
 
 require_once(ROOT_DIR . '/vendor/autoload.php');
 
