62eaee349f9d3885e0a9950e4fe595edda1466f2
Kilian LE DU <ledu.kilian@gmail.com>
2021-06-10 15:55:48 +0200

    Added twig pages

--- src/Controllers/IndexController.php
+++ src/Controllers/IndexController.php
@@ -6,12 +6,10 @@ use App\Core\Controller;
 class IndexController extends Controller {
    public function showHome() {
       $this->render("@client/pages/index.html.twig", []);
-
    }
 
    public function showContact() {
-      echo 'Contact.';
-      // Ici on ira chercher la vue de contact
+      $this->render("@client/pages/contact.html.twig", []);
    }
 
 
