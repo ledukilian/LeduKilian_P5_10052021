16dfbf4fdce43814f196a6b378c3b3329d591526
ledukilian <ledu.kilian@gmail.com>
2021-06-14 19:57:31 +0200

    Index OK

--- src/Controllers/IndexController.php
+++ src/Controllers/IndexController.php
@@ -11,6 +11,10 @@ class IndexController extends Controller {
    public function showContact() {
       $this->render("@client/pages/contact.html.twig", []);
    }
+   
+   public function showPortfolio() {
+      $this->render("@client/pages/portfolio.html.twig", []);
+   }
 
 
 
