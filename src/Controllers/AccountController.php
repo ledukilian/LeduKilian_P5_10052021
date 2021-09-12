50ef100081d6443c744f11f9c8d239b64c091daf
Kilian LE DU <ledu.kilian@gmail.com>
2021-09-12 23:41:12 +0200

    Added function for session creation

--- src/Controllers/AccountController.php
+++ src/Controllers/AccountController.php
@@ -2,13 +2,36 @@
 namespace App\Controllers;
 
 use App\Core\Controller;
+use App\Managers\UserManager;
+use App\Managers\PostManager;
 
 
 class AccountController extends Controller {
 
 
    public function login() {
-      $this->render("@client/pages/login.html.twig", []);
+      if (isset($_POST['email'])) {
+         $userManager = new UserManager();
+         if ($userManager->tryLogin()!=FALSE) {
+            $postManager = new PostManager();
+            $posts = $postManager->findBy(
+               [],
+               [
+                  'created_at' => 'DESC'
+               ],
+               3
+            );
+            $this->render("@client/pages/index.html.twig", [
+               'posts' => $posts
+            ]);
+         }
+      } else {
+         $this->render("@client/pages/login.html.twig", []);
+      }
+      // TODO : Concordance password
+      // TODO : Redirections
+      // TODO : Ajout de l'utilisateur dans TwigGlobals
+      // TODO : Adaptation des template : Lien de déconnexion, Formulaire connexion etc..
    }
 
    public function register() {
--- src/Managers/UserManager.php
+++ src/Managers/UserManager.php
@@ -2,14 +2,32 @@
 namespace App\Managers;
 
 use App\Core\Manager;
+use App\Models\User;
 use PDO;
 
 class UserManager extends Manager {
 
+   public function tryLogin() {
+      $email = $_POST['email'];
+      $password = $_POST['password'];
+      $sql = "SELECT * FROM user WHERE user.email = '$email'";
+      $result = ($this->db)->query($sql)->fetch(PDO::FETCH_ASSOC);
+      if (!empty($result)) {
+         if ($password==$result['password']) {
+            self::createSession($result);
+            return true;
+         } else {
+            return false;
+         }
+      } else {
+         return null;
+      }
+   }
 
-
-
-
+   public function createSession(Array $userElements) {
+      $_SESSION['user'] = new User($userElements);
+      $_SESSION['loginDate'] = date('Y-m-d H:i:s');
+   }
 
 
 }
