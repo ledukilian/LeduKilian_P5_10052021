e110edc5c6dbddaef6fe8f280e9ab76e1192ed17
Kilian LE DU <ledu.kilian@gmail.com>
2021-09-13 02:02:15 +0200

    Added password hash

--- src/Managers/UserManager.php
+++ src/Managers/UserManager.php
@@ -14,7 +14,8 @@ class UserManager extends Manager {
       $query->execute();
       $result = $query->fetch(PDO::FETCH_ASSOC);
       if (!empty($result)) {
-         if ($password==$result['password']) {
+         if (password_verify($password, $result['password'])) {
+         //if ($password==$result['password']) {
             self::createSession($result);
             return true;
          } else {
@@ -30,6 +31,36 @@ class UserManager extends Manager {
       $_SESSION['loginDate'] = date('Y-m-d H:i:s');
       $_SESSION['logged'] = TRUE;
    }
+
+   public function createUser() {
+      $_POST['username'] = htmlspecialchars($_POST['username']);
+      $_POST['email'] = htmlspecialchars($_POST['email']);
+      $_POST['firstname'] = htmlspecialchars($_POST['firstname']);
+      $_POST['lastname'] = htmlspecialchars($_POST['lastname']);
+      $query = ($this->db)->prepare("INSERT INTO user
+         VALUES (DEFAULT, :username, :lastname, :firstname, :email, :password, 'USER', NOW(), NOW())");
+      $query->bindParam(':username', $_POST['username'], PDO::PARAM_STR);
+      $query->bindParam(':lastname', $_POST['lastname'], PDO::PARAM_STR);
+      $query->bindParam(':firstname', $_POST['firstname'], PDO::PARAM_STR);
+      $query->bindParam(':email', $_POST['email'], PDO::PARAM_STR);
+      $query->bindParam(':password', password_hash($_POST['password'], PASSWORD_DEFAULT), PDO::PARAM_STR);
+      return $query->execute();
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
