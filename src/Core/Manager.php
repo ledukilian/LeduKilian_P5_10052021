2bc3cd0d4d94b3d901f32fb86a909488e99e117d
Kilian LE DU <ledu.kilian@gmail.com>
2021-10-11 23:15:18 +0200

    Added slugify function

--- src/Core/Manager.php
+++ src/Core/Manager.php
@@ -128,5 +128,17 @@ class Manager {
         return implode(', ', $valuesPlaceholder);
     }
 
+    public static function slugify($text, string $divider = '-') {
+      $text = preg_replace('~[^\pL\d]+~u', $divider, $text);
+      $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+      $text = preg_replace('~[^-\w]+~', '', $text);
+      $text = trim($text, $divider);
+      $text = preg_replace('~-+~', $divider, $text);
+      $text = strtolower($text);
+      if (empty($text)) {
+        return 'n-a';
+      }
+      return $text;
+    }
 
 }
