fdffcfc8962618b5bceca7926c01aaf5b9311eb8
Kilian LE DU <ledu.kilian@gmail.com>
2021-10-11 23:16:02 +0200

    Added insert route for comment

--- src/Controllers/CommentController.php
+++ src/Controllers/CommentController.php
@@ -2,13 +2,16 @@
 namespace App\Controllers;
 
 use App\Core\Controller;
+use App\Managers\CommentManager;
+use App\Models\Comment;
+use App\Services\FormHandler;
 
 
 class CommentController extends Controller {
 
    public function toggleCommentStatus() {
       // TODO : Si commentaire statut = 1 On désactive
-   
+
 
 
 
@@ -17,7 +20,22 @@ class CommentController extends Controller {
 
    }
 
-
+public function addComment() {
+   if (isset($_POST)) {
+      if ((new FormHandler())->checkform($_POST)) {
+         $commentManager = new CommentManager();
+         $_POST['_userId'] = $_SESSION['user']->getId();
+         $_POST['_status'] = 1;
+         // if ($postManager->insert(new Post($_POST))) {
+         //
+         // } else {
+         //
+         // }
+         var_dump(new Comment($_POST));
+         die;
+      }
+   }
+}
 
 
 
