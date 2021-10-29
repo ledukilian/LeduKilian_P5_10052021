fd5d45dec3c5ba09a5c862c45ceb099df17ed4aa
Kilian LE DU <ledu.kilian@gmail.com>
2021-10-29 14:44:06 +0200

    Added getAdminId function

--- src/Services/TwigGlobals.php
+++ src/Services/TwigGlobals.php
@@ -8,11 +8,15 @@ class TwigGlobals {
 
    public function getAdmin() {
       $adminManager = new AdminManager();
-      return $adminManager->findAdminBy(1);
+      return $adminManager->findAdminBy($this->getAdminId());
+   }
+
+   public function getAdminId() {
+      return 1;
    }
 
    public function getSession() {
-         return $_SESSION;
+      return $_SESSION;
    }
 
    public function isConnected() {
@@ -23,7 +27,7 @@ class TwigGlobals {
       $socialManager = new SocialManager();
       $socials = $socialManager->findBy(
          [
-            'id_admin' => 1
+            'id_admin' => $this->getAdminId()
          ]
       );
       if (empty($socials)) {
@@ -34,7 +38,7 @@ class TwigGlobals {
 
    public function getPortfolio() {
       $adminManager = new AdminManager();
-      $portfolio = $adminManager->findAdminBy(1);
+      $portfolio = $adminManager->findAdminBy($this->getAdminId());
       return $portfolio;
    }
 
