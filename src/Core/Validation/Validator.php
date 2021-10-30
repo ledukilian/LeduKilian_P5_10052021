016eca80491d261e518a049c7ab375d7000f911f
Kilian LE DU <ledu.kilian@gmail.com>
2021-10-30 09:20:32 +0200

    Added Validator class

---
+++ src/Core/Validation/Validator.php
@@ -0,0 +1,14 @@
+<?php
+
+
+
+class Validator {
+
+   // 1 fonction pour chaque formulaire faisant appel aux contraintes
+
+
+   public function checkAdminForm() {
+      // Si mot de passe vide : on l'enlève du post
+   }
+
+}
