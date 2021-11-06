db80288784af8f527729f9dc554ed72d56813f1f
ledukilian <ledu.kilian@gmail.com>
2021-11-06 10:42:35 +0100

    Work on Validator class

--- src/Core/Validation/Validator.php
+++ src/Core/Validation/Validator.php
@@ -1,26 +1,94 @@
 <?php
+namespace App\Core\Validation;
 
-
+use App\Core\Validation\ValidatorConstraint;
+use App\Services\MessageHandler;
 
 class Validator {
+   protected MessageHandler $messageHandler;
    private ValidatorConstraint $validator;
    protected Array $data;
+   public static $TEXTS = [
+      'number' => ' doit comporter au moins un caractère numérique'
+   ];
+   public static $FIELDS = [
+      'password' => ' mot de passe '
+   ];
 
    public function __construct(Array $data, $manager = null) {
+      $this->messageHandler = new MessageHandler();
       $this->data = $data;
       $this->validator = new ValidatorConstraint($this->data, $manager);
    }
 
-   // Validation de base, fonction basicValidation
-   // 1 fonction par formulaire
+   public function getErrors() {
+      return $this->validator->getErrors();
+   }
+
+   public function isValid() {
+      if (sizeof($this->getErrors())==0) {
+         return true;
+      } else {
+         return false;
+      }
+   }
 
    public function basicValidation() {
-      $this->validator->allNotEmpty($keys)
-                      ->allRequired($keys);
+      return $this->validator->allNotEmpty()
+                             ->allRequired();
    }
 
-   public function checkAdminForm() {
-      // Si mot de passe vide : on l'enlève du post
+   public function checkRegister() {
+      $this->basicValidation()
+           ->email('email')
+           ->password('password')
+           ->containsAlphabet('lastname')
+           ->containsAlphabet('firstname');
+      $this->showMessagesFromErrors();
+      return $this;
    }
+
+   public function checkLogin() {
+      $this->basicValidation()
+           ->email('email')
+           ->password('password');
+      $this->showMessagesFromErrors();
+      return $this;
+   }
+
+   public function showMessagesFromErrors() {
+      foreach ($this->getErrors() as $key => $value) {
+         $this->messageHandler->setMessage('danger', 'Le champ '.self::$FIELDS[$key].self::$TEXTS[$value]);
+      }
+   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
