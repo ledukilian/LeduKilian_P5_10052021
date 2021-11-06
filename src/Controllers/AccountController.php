44f6ca8d2957ee380a87177a5615574ee68dc302
ledukilian <ledu.kilian@gmail.com>
2021-11-06 10:42:56 +0100

    Added Validation for AccountController

--- src/Controllers/AccountController.php
+++ src/Controllers/AccountController.php
@@ -6,11 +6,12 @@ use App\Managers\UserManager;
 use App\Managers\PostManager;
 use App\Models\User;
 use App\Services\MessageHandler;
-
+use App\Core\Validation\Validator;
 
 class AccountController extends Controller {
    protected MessageHandler $messageHandler;
    protected UserManager $userManager;
+   protected Validator $validator;
 
    public function __construct($action, $params = NULL) {
       parent::__construct($action, $params);
@@ -19,13 +20,18 @@ class AccountController extends Controller {
    }
 
    public function login() {
-      if (isset($_POST['email'])) {
-         $try = $this->userManager->tryLogin();
-         if (is_object($try)) {
-            $this->messageHandler->setMessage('success', 'Connexion réussie ! Vous êtes maintenant connecté.');
-            $this->redirectToIndex();
+      if (!empty($_POST['email'])) {
+         $this->validator = new Validator($_POST);
+         if ($this->validator->checkLogin()->isValid()) {
+            $try = $this->userManager->tryLogin();
+            if (is_object($try)) {
+               $this->messageHandler->setMessage('success', 'Connexion réussie ! Vous êtes maintenant connecté.');
+               $this->redirectToIndex();
+            } else {
+               $this->messageHandler->setMessage('danger', $try);
+               $this->redirectToLogin();
+            }
          } else {
-            $this->messageHandler->setMessage('danger', $try);
             $this->redirectToLogin();
          }
       }
@@ -33,15 +39,18 @@ class AccountController extends Controller {
    }
 
    public function register() {
-      if (isset($_POST['email'])) {
-         $user = new User($_POST);
-         $user->setRole("USER");
-         $user->setPasswordHashed($user->getPassword());
-         if ($this->userManager->insert($user)) {
-            $this->messageHandler->setMessage('success', 'Création de compte réussie, veuillez vous connecter.');
-            $this->redirectToIndex();
-         } else {
-            $this->messageHandler->setMessage('danger', 'Une erreur est survenue lors de l\'inscription.');
+      if (!empty($_POST['email'])) {
+         $this->validator = new Validator($_POST);
+         if ($this->validator->checkRegister()->isValid()) {
+            $user = new User($_POST);
+            $user->setRole("USER");
+            $user->setPasswordHashed($user->getPassword());
+            if ($this->userManager->insert($user)) {
+               $this->messageHandler->setMessage('success', 'Création de compte réussie, veuillez vous connecter.');
+               $this->redirectToIndex();
+            } else {
+               $this->messageHandler->setMessage('danger', 'Une erreur est survenue lors de l\'inscription.');
+            }
          }
       }
       $this->render("@client/pages/register.html.twig", []);
