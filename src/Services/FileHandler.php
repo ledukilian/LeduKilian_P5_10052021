d2c9d451f291af01cc46d70a54e573df20562b4a
Kilian LE DU <ledu.kilian@gmail.com>
2021-11-12 10:31:34 +0100

    Progress on FileHandler

--- src/Services/FileHandler.php
+++ src/Services/FileHandler.php
@@ -2,10 +2,32 @@
 namespace App\Services;
 
 class FileHandler {
+   private static $max_size;
+   private static $formats;
+   protected Array $file;
 
+   public function __construct() {
+      if (!empty($_FILES)) {
+         $this->file = $_FILES[0];
+      }
+      self::max_size = 200000;
+      self::formats = [
+         'image/jpeg',
+         'image/png'
+      ];
+   }
 
+public function check() {
+      $this->checkFormat()
+            ->checkSize()
+            ->generateName();
 
+      if () {
+         return $path;
+      }
 
+}
 
+public function checkSize() {
 
 }
